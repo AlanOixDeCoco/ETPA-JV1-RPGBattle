d8c2f41c22f86aada9d41b7c09b2c76bcb260a57
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2022-11-24 17:00:10 +0100

    oe

--- source.js
+++ source.js
@@ -1,1 +1,9 @@
+var characters = ["character_1", "character_2", "character_3", "character_4"];
+var ennemies = [["ennemy_1", ""], "ennemy_2", "ennemy_3"];
+
 document.getElementById("mana_bar").style.width = "40%"; // changer le remplissage de la barre
+
+document.getElementById(characters[3]).getElementsByClassName("cursor")[0].style.visibility = "visible"; // pour afficher un curseur
+
+document.getElementById("character_frame").getElementsByTagName('img')[0].src="Assets/" + characters[3] + ".png"; // pour placer un personnage dans le cadre
+
