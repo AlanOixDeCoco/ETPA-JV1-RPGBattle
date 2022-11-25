e7d72dc57b9dc945d02e68db1d851f5d4b99d242
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2022-11-25 18:12:48 +0100

    switched ennemies cursors

--- source.js
+++ source.js
@@ -1,5 +1,15 @@
-var characters = ["character_1", "character_2", "character_3", "character_4"];
-var ennemies = [["ennemy_1", ""], "ennemy_2", "ennemy_3"];
+var characters = [
+    ["character_1", "Giro Smileur", 100, 10], // id html, nom, points de vie, dégats
+    ["character_2", "Turbo Incognito", 100, 10], 
+    ["character_3", "Ultra Cowboy", 100, 10], 
+    ["character_4", "Giga Chad", 100, 10]
+];
+
+var ennemies = [
+    ["ennemy_1", "Clown", 200, 20], // id html, nom, points de vie, dégats
+    ["ennemy_2", "Ogre", 200, 20], 
+    ["ennemy_3", "Goblin", 200, 20]
+];
 
 document.getElementById("mana_bar").style.width = "40%"; // changer le remplissage de la barre
 
