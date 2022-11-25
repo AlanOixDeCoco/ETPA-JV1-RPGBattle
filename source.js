c3b69ae74db289b707f8e1dd57acd1754795e9e6
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2022-11-25 19:50:21 +0100

    Organize js script

--- source.js
+++ source.js
@@ -1,19 +1,61 @@
+//#region constants
+// Indexes constants
+const ID = 0; // characters/ennemies ID index in arrays
+const NAME = 1; // characters/ennemies NAME index in arrays
+const HP = 2; // characters/ennemies HP index in arrays
+const DMG = 3; // characters/ennemies DMG (pts) index in arrays
+const MANA = 4; // characters MANA index in arrays
+const SPE = 5; // characters SPECIAL ABILITY index in arrays
+
+// Values constants : characters
+const MAX_CHARACTERS_HEALTH = 100; // max characters HP
+const MAX_CHARACTERS_MANA = 100; // max characters MANA
+const MAX_CHARACTERS_DAMAGE = 20; // maximum damage to deal to the ennemies
+
+// Values constants : ennemies
+const MAX_ENNEMIES_HEALTH = 150; // max ennemies HP
+const MIN_ENNEMIES_DAMAGE = 10; // minimum damage to deal to the characters
+//#endregion
+
+//#region characters & ennemies arrays
 var characters = [
-    ["character_1", "Giro Smileur", 100, 10], // id html, nom, points de vie, dégats
-    ["character_2", "Turbo Incognito", 100, 10], 
-    ["character_3", "Ultra Cowboy", 100, 10], 
-    ["character_4", "Giga Chad", 100, 10]
+    ["character_1", "Giro Smileur", MAX_CHARACTERS_HEALTH, 10, MAX_CHARACTERS_MANA, heal], // html ID, NAME, HP (health pts), DMG (damage pts), MANA, SPE (special ability function)
+    ["character_2", "Turbo Incognito", MAX_CHARACTERS_HEALTH, 10, MAX_CHARACTERS_MANA, heal], 
+    ["character_3", "Ultra Cowboy", MAX_CHARACTERS_HEALTH, 10, MAX_CHARACTERS_MANA, heal], 
+    ["character_4", "Giga Chad", MAX_CHARACTERS_HEALTH, 10, MAX_CHARACTERS_MANA, heal]
 ];
 
 var ennemies = [
-    ["ennemy_1", "Clown", 200, 20], // id html, nom, points de vie, dégats
-    ["ennemy_2", "Ogre", 200, 20], 
-    ["ennemy_3", "Goblin", 200, 20]
+    ["ennemy_1", "Clown", MAX_ENNEMIES_HEALTH, 15], // html ID, NAME, HP (health pts), DMG (damage pts),
+    ["ennemy_2", "Ogre", MAX_ENNEMIES_HEALTH, 15], 
+    ["ennemy_3", "Goblin", MAX_ENNEMIES_HEALTH, 15]
 ];
+//#endregion
 
-document.getElementById("mana_bar").style.width = "40%"; // changer le remplissage de la barre
+//#region characters special abilities
+// HEAL: Adds 'amount' hp to the 'target', and returns the action description sentence
+function heal(target, amount){
+    target[HP] += amount;
+    if(target[HP] > MAX_CHARACTERS_HEALTH){
+        target[HP] = MAX_CHARACTERS_HEALTH;
+    }
+    return `${target[NAME]} a reçu ${amount} PV de soin !\n${target[NAME]} a désormais ${target[HP]} PV.`
+}
 
-document.getElementById(characters[3]).getElementsByClassName("cursor")[0].style.visibility = "visible"; // pour afficher un curseur
+// REDUCE DAMAGE: Removes 'amount' to the 'target' damage pts, and returns the action description sentence
+function reduceDamage(target, amount){
+    target[DMG] -= amount;
+    if(target[DMG] < MIN_ENNEMIES_DAMAGE){
+        target[DMG] = MIN_ENNEMIES_DAMAGE;
+    }
+    return `Dégats de ${target[NAME]} réduits de ${amount} !\n${target[NAME]} inflige désormais ${target[DMG]} pts de dégat.`
+}
+//#endregion
 
-document.getElementById("character_frame").getElementsByTagName('img')[0].src="Assets/" + characters[3] + ".png"; // pour placer un personnage dans le cadre
 
+// just some testing code
+document.getElementById("mana_bar").style.width = "20%"; // changer le remplissage de la barre
+
+document.getElementById(characters[3][0]).getElementsByClassName("cursor")[0].style.visibility = "visible"; // pour afficher un curseur
+
+document.getElementById("character_frame").getElementsByTagName('img')[0].src="Assets/" + characters[3][0] + ".png"; // pour placer un personnage dans le cadre
